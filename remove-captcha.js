874141fa5e5c3427efa820e08f5f8db56daeb3fb
Kowalski Dark <darkkowalski2012@gmail.com>
2019-03-15 00:49:12 +0800

    format

--- remove-captcha.js
+++ remove-captcha.js
@@ -14,8 +14,8 @@
     document.getElementsByTagName('strong')[0].innerHTML = "迫 真 身 份 认 证 ?";
 
     // Submit form by yourself
-    document.getElementsByName("btsubmit")[0].value="MDZZ! Login!";
-    document.getElementsByName("btsubmit")[0].onclick= function() {document.forms[0].submit()};
+    document.getElementsByName("btsubmit")[0].value = "MDZZ! Login!";
+    document.getElementsByName("btsubmit")[0].onclick = function() {document.forms[0].submit()};
 
     // Remove Comical CAPTCHA box
     var txticode = document.getElementById("Txtidcode");
